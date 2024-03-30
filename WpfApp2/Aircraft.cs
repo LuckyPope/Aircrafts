8a0ad657f27c414c4cfa34b20f07f67b048942a0
luckypope <zhilyaev_18@mail.ru>
2024-03-30 17:36:03 +0300

    Initial commit

---
+++ WpfApp2/Aircraft.cs
@@ -0,0 +1,17 @@
+namespace WpfApp2;
+
+public abstract class Aircraft
+{
+    protected int FlightAltitude;
+    protected bool IsNeedRunway;
+
+    protected Aircraft(int flightAltitude, bool isNeedRunway)
+    {
+        this.FlightAltitude = flightAltitude;
+        this.IsNeedRunway = isNeedRunway;
+    }
+
+    public abstract bool Takeoff(int runwayLength);
+
+    public abstract bool Boarding(int runwayLength);
+}
